ba62976473bc7f8764fbd0c2253dd3654d7260b6
mattdood <mawimberly@csumb.edu>
2020-10-08 22:07:49 -0700

    bot general setup for testing and development, basic BrandoBot added

--- bot.py
+++ bot.py
@@ -2,19 +2,35 @@ import os
 import discord
 import settings
 
-if __name__ == '__main__':
-    client = discord.Client()
+from commands import General
+
+intents = discord.Intents.default()
+intents.members = True
+
+class BrandoBot(commands.Bot):
     
-    @client.event
-    async def on_ready():
-        print(f'{client.user} has connected to Discord')
-        guild = discord.utils.get(client.guilds, name=settings.DISCORD_GUILD)
-        
+    async def on_ready(self):
+        """
+        Logging information to server on startup
+        """
+        guild = discord.utils.get(bot.guilds, name=settings.DISCORD_GUILD)
+        print(f'{bot.user} has connected to Discord')    
         print(
-            f'{client.user} is connected to the following guild:\n'
+            f'{bot.user} is connected to the following guild:\n'
             f'{guild.name}(id: {guild.id})\n'
         )
         members = '\n - '.join([member.name for member in guild.members])
-        print(f'Guild Members:\n - {members}')
-    
-    client.run(settings.DISCORD_TOKEN)
+        print(f'Guild Members:\n - {members}')  
+
+    async def on_member_join(self, member):
+        guild = member.guild
+        if guild.system_channel is not None:
+            msg = '{member.mention} - you thought this was a welcome message, but it was I! Dio!'.format(member=member)
+        await guild.system_channel.send(msg)
+
+
+bot = BrandoBot(command_prefix='!', intents=intents)
+
+bot.add_cog(General(bot))
+
+bot.run(settings.DISCORD_TOKEN)
