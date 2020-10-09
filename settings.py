9af72571ca120dd8f7a5669ffa29416549767352
mattdood <mawimberly@csumb.edu>
2020-10-08 20:15:31 -0700

    configured settings with guild

--- settings.py
+++ settings.py
@@ -10,6 +10,9 @@ ROOT_DIR = Path(__file__).resolve(strict=True).parent
 
 ENV_PATH = join(ROOT_DIR, ".env/.production/.secrets")
 
+# Loads the `.secrets` file
 load_dotenv(ENV_PATH)
 
+# Discord secrets
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+DISCORD_GUILD = os.getenv("DISCORD_GUILD")
