185f4f57d4669bdac1c26840dc6575800e626866
mattdood <mawimberly@csumb.edu>
2020-10-08 20:15:55 -0700

    refactored init, gave guild baseline

--- bot.py
+++ bot.py
@@ -2,10 +2,19 @@ import os
 import discord
 import settings
 
-client = discord.Client()
-
-@client.event
-async def on_ready():
-    print(f'{client.user} has connected to Discord')
-
-client.run(settings.DISCORD_TOKEN)
+if __name__ == '__main__':
+    client = discord.Client()
+    
+    @client.event
+    async def on_ready():
+        print(f'{client.user} has connected to Discord')
+        guild = discord.utils.get(client.guilds, name=settings.DISCORD_GUILD)
+        
+        print(
+            f'{client.user} is connected to the following guild:\n'
+            f'{guild.name}(id: {guild.id})\n'
+        )
+        members = '\n - '.join([member.name for member in guild.members])
+        print(f'Guild Members:\n - {members}')
+    
+    client.run(settings.DISCORD_TOKEN)
